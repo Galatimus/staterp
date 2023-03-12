2e0e56d160532d602077403444ebef31a5fecdd8
Mykola Dovhopol <ua.lifesheets@gmail.com>
2023-03-12 12:47:14 +0200

    Original files cefbrowser mod interface.
    npm install --legacy-peer-deps

---
+++ interface/copyItem.ps1
@@ -0,0 +1,3 @@
+Remove-Item "../client_packages/game_resources/interface/*" -Recurse -force
+Copy-Item "./build/*" -Destination "../client_packages/game_resources/interface" -Recurse -force
+Remove-Item "./build/*" -Recurse -force 
