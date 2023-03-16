e9fa70e46ad44d980f64a30bc4c79acec8dbc452
Mykola Dovhopol <ua.lifesheets@gmail.com>
2023-03-16 16:52:48 +0200

    [Add] We turn off the example by introducing ragemp 1.1

--- handlers/src/manager/timer.js
+++ handlers/src/manager/timer.js
@@ -1021,6 +1021,15 @@ timer.loadAll = function () {
                 mp.game.controls.disableControlAction(2, 257, true);
                 mp.game.controls.disableControlAction(2, 263, true);
             }
+            // Вимикаємо приклад, представляючи ragemp 1.1
+            if (user.getCurrentWeapon() != -1569615261) {
+                mp.game.controls.disableControlAction(2, 140, true);
+                mp.game.controls.disableControlAction(2, 141, true);
+                mp.game.controls.disableControlAction(2, 142, true);
+                mp.game.controls.disableControlAction(2, 143, true);
+                mp.game.controls.disableControlAction(2, 263, true);
+                mp.game.controls.disableControlAction(2, 264, true);
+            }
         }
         catch (e) {}
     }, 5);
