c941497ab571d4a358ba7dc4f6d97d8b7b28b383
Mykola Dovhopol <ua.lifesheets@gmail.com>
2023-03-20 11:08:29 +0200

    [Add] Turn off ambient sounds on the server.

--- handlers/src/index.js
+++ handlers/src/index.js
@@ -52,6 +52,8 @@ mp.events.add('playerReady', player => {
 try {
     // Автоматичний парашут при виході з повітряного т/с
     mp.game.player.setAutoGiveParachuteWhenEnterPlane(true); 
+    // Відключаємо навколишні звуки на сервері.
+    mp.game.audio.startAudioScene("CHARACTER_CHANGE_IN_SKY_SCENE");
 
     user.showLoadDisplay();
     user.setVirtualWorld(mp.players.local.remoteId);
