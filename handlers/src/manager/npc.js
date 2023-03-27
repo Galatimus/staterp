834166ccb4e9894cdab4e80a3937c07f87e21bab
Mykola Dovhopol <ua.lifesheets@gmail.com>
2023-03-27 19:40:15 +0300

    [Edit] Moved vehicle rental to Pillbox Hill / Integrity Way

--- handlers/src/manager/npc.js
+++ handlers/src/manager/npc.js
@@ -261,8 +261,7 @@ npc.loadAll = function() {
     npc.create(mp.game.joaat("a_m_m_skater_01"), new mp.Vector3(1806.736, 3676.029, 34.27676), -39.58422, false, "WORLD_HUMAN_SMOKING");
 // Bike rent - Болингброк - Шоссе 68
     npc.create(mp.game.joaat("a_m_y_skater_01"), new mp.Vector3(1852.597, 2594.885, 45.67204), -80.29984, false, "WORLD_HUMAN_AA_COFFEE");
-// Bike rent - Текстайл-Сити - Элгин-авеню
-    npc.create(mp.game.joaat("a_f_y_skater_01"), new mp.Vector3(297.7727, -607.1932, 43.37174), 88.88361, false, "CODE_HUMAN_MEDIC_TIME_OF_DEATH");
+    npc.create(mp.game.joaat("a_f_y_skater_01"), new mp.Vector3(284.9119, -610.1529, 43.3058), 43.3, false, "CODE_HUMAN_MEDIC_TIME_OF_DEATH");  // Bike rent - Миррор-Парк - Никола-авеню
 // Bike rent - Миррор-Парк - Никола-авеню
     npc.create(mp.game.joaat("a_m_m_skater_01"), new mp.Vector3(1153.488, -454.4688, 66.98437), 170.2867, false, "WORLD_HUMAN_AA_COFFEE");
 // Bike rent - Пиллбокс-Хилл - Бульвар Веспуччи
